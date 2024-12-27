be3d8771ace56233b58834d5d5f89629ea7798ad
Marcus <marcusvlcastro@gmail.com>
2024-12-27 15:59:01 +0100

    11509 - novo carrossel de marcas

--- sections/Images/BrandGallery.tsx
+++ sections/Images/BrandGallery.tsx
@@ -3,15 +3,18 @@ import {
   MOBILE_COLUMNS,
 } from "$store/components/ui/BannerGrid.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
-import Image from "apps/website/components/Image.tsx";
 import { Head } from "$fresh/runtime.ts";
-
+import Slider from "../../components/ui/Slider.tsx";
+import SliderJS from "../../islands/SliderJS.tsx";
+import { useId } from "preact/hooks";
+import Icon from "../../components/ui/Icon.tsx";
+import { AppContext } from "site/apps/site.ts";
+import Image from "apps/website/components/Image.tsx";
 export type ResponsiveConditionals =
   | "Always"
   | "Desktop Only"
   | "Mobile Only"
   | "Never";
-
 export const CONDITIONAL_RESPONSIVE_PARAMS: Record<
   ResponsiveConditionals,
   string
@@ -22,21 +25,18 @@ export const CONDITIONAL_RESPONSIVE_PARAMS: Record<
   "Never": "hidden",
 };
 export interface ImageGalleryItem {
-
+  /** @description Title */
+  title?: string;
   /** @description Image url */
   image: LiveImage;
-
   /** @description Alt text */
   alt: string;
-
   /** @description Link */
   href?: string;
-
   /** @description Abrir em nova página? */
   blank?: boolean;
   preload?: boolean;
 }
-
 export interface Props {
   /** @description Banners */
   images: ImageGalleryItem[];
@@ -46,9 +46,7 @@ export interface Props {
   itemPerPageMobile?: 1 | 2;
   /** @description Enable hover effect on images */
   hoverEffect?: boolean;
-
   device?: string;
-
   interval?: number;
   /**
    * @title Show pagination arrows?
@@ -60,48 +58,158 @@ export interface Props {
    */
   showPaginationDots?: ResponsiveConditionals;
 }
+interface DotsProps {
+  images?: ImageGalleryItem[];
+  /**
+   * @description Check this option when this banner is the biggest image on the screen for image optimizations
+   */
+  interval?: number;
+  /**
+   * @title Show pagination arrows?
+   */
+  className: string;
+}
+function Dots({ images, className, interval = 0 }: DotsProps) {
+  return (
+    <>
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+          @property --dot-progress {
+            syntax: '<percentage>';
+            inherits: false;
+            initial-value: 0%;
+          }
+          `,
+        }}
+      />
+      <ul
+        class={`carousel justify-center col-span-full gap-2 z-10 row-start-4 ${className}`}
+      >
+        {images?.map((_, index) => (
+          <li class="carousel-item">
+            <Slider.Dot index={index}>
+              <div class="py-5">
+                <div
+                  class="w-4 h-4 group-disabled:opacity-100 opacity-20 rounded-full bg-primary"
+                  style={{ animationDuration: `${interval}s` }}
+                />
+              </div>
+            </Slider.Dot>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+interface ButtonsProps {
+  className: string;
+}
+function Buttons({ className }: ButtonsProps) {
+  return (
+    <>
+      <div
+        class={`flex items-center justify-center z-20  col-start-1 row-start-2 ${className}`}
+      >
+        <Slider.PrevButton class="btn btn-circle border-none shadow-md bg-white lg:opacity-60 lg:hover:bg-white lg:hover:opacity-100">
+          <Icon
+            class="text-primary"
+            size={32}
+            id="LeftArrowBanner"
+          />
+        </Slider.PrevButton>
+      </div>
+      <div
+        class={`flex items-center justify-center z-20 col-start-3 row-start-2 ${className}`}
+      >
+        <Slider.NextButton class="btn btn-circle border-none shadow-md bg-white lg:opacity-60 lg:hover:bg-white lg:hover:opacity-100">
+          <Icon
+            class="text-primary"
+            size={32}
+            id="RightArrowBanner"
+          />
+        </Slider.NextButton>
+      </div>
+    </>
+  );
+}
 
 export default function BrandGallery(props: Props) {
   const {
     images,
     itemPerPageMobile = 2,
     itemPerPageDesktop = 3,
-    hoverEffect
+    hoverEffect, showPaginationArrows,
+    showPaginationDots,
+    interval,
+
   } = props;
+
+  const id = useId();
 
   return (
     <section
       class={`w-full h-full lg:grid justify-center lg:gap-4 gap-5 my-12 max-md:my-8 flex flex-wrap ${MOBILE_COLUMNS[itemPerPageMobile ?? 2]
         }} ${DESKTOP_COLUMNS[itemPerPageDesktop ?? 3]}`}
     >
-      {images.map((item) => (
-        <a
-          target={item.blank ? "_self" : "_blank"}
-          href={item.href}
-          class="relative overflow-hidden rounded-xl w-[45%] lg:w-full m-auto group flex flex-col-reverse items-center"
-        >
-          {item.preload && (
-            <Head>
-              <link
-                as="image"
-                rel="preload"
-                href={item.image}
+      <Slider class="carousel sm:gap-0 sm:carousel-center col-span-full row-[1/5]">
+        {images.map((item, index) => (
+          <Slider.Item
+            index={index}
+            class={`carousel-item lg:flex lg:justify-center !w-1/2	 ${MOBILE_COLUMNS[itemPerPageMobile ?? 2]
+              } ${DESKTOP_COLUMNS[itemPerPageDesktop ?? 3]}`}
+          >
+            <a
+              target={item.blank ? "_self" : "_blank"}
+              href={item.href}
+              class="relative overflow-hidden rounded-xl w-[80%] lg:w-full m-auto group flex flex-col-reverse items-center"
+            >
+              {item.title && (
+                <span class=" text-primary bottom-0 mb-[30px] px-10 py-[17px] left-1/2 z-30 max-sm:text-[12px] text-[19px] lg:text-[19px] font-bold whitespace-nowrap">
+                  {item.title}
+                </span>
+              )}
+              {item.preload && (
+                <Head>
+                  <link
+                    as="image"
+                    rel="preload"
+                    href={item.image}
+                  />
+                </Head>
+              )}
+              <img
+                preload={undefined}
+                loading={"lazy"}
+                class={`w-full h-full scale-100 ${hoverEffect ? "group-hover:scale-110" : ""
+                  } transition-all duration-700`}
+                src={item.image}
+                alt={"imagem de " + item.alt}
+                decoding="async"
+                width={300}
+                height={300}
               />
-            </Head>
-          )}
-          <Image
-            preload={undefined}
-            loading={"lazy"}
-            class={`w-full h-full scale-100 ${hoverEffect ? "group-hover:scale-110" : ""
-              } transition-all duration-700`}
-            src={item.image}
-            alt={"imagem de " + item.alt}
-            decoding="async"
-            width={300}
-            height={300}
-          />
-        </a>
-      ))}
+            </a>
+          </Slider.Item>
+        ))}
+      </Slider>
+      <Buttons
+        className={CONDITIONAL_RESPONSIVE_PARAMS[
+          showPaginationArrows ? showPaginationArrows : "Always"
+        ]}
+      />
+      <Dots
+        images={images}
+        interval={interval}
+        className={CONDITIONAL_RESPONSIVE_PARAMS[
+          showPaginationDots ? showPaginationDots : "Always"
+        ]}
+      />
+      <SliderJS
+        rootId={id}
+        interval={interval && interval * 1e3}
+        infinite
+      />
     </section>
   );
 }
