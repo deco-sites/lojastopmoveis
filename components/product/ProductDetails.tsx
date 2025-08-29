44bf928deabdc0c6df349731da5ec22bc1217d19
marcusvl <marcusvlcastro@gmail.com>
2025-08-29 11:06:04 -0300

    12475 - fix: removido o texto no pix da flag de desconto

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -213,7 +213,7 @@ function ProductInfo({ page, shareableNetworks, tags, reviews, aggregateRating }
               </span>
               {discount && forPrice && (
                 <span class="font-bold max-lg:text-[10px] max-lg:px-[5px] text-[12px] border border-[#4A4B51] rounded-md text-[#4A4B51] py-[2px] tracking-[2px] px-[10px] ">
-                  {Math.round(((forPrice - price) / forPrice) * 100)}% off no Pix
+                  {Math.round(((forPrice - price) / forPrice) * 100)}% off
                 </span>
               )}
             </div>
